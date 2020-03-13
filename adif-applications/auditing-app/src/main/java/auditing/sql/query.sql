6182ff2cfc92fd4805ced333e2d8b260524fed22
Shakir Azimli <azimlishakir@gmail.com>
2020-03-13 17:39:55 +0400

    Sql file elave edildi

---
+++ adif-applications/auditing-app/src/main/java/auditing/sql/query.sql
@@ -0,0 +1,4 @@
+select * from auditlogin;
+
+INSERT INTO auditlogin (id, email, lastname, name, password, status, username)
+VALUES (1,'azimlishakir@gmail.com','Azimli','Shakir','123456',1,'shakir.azimli');
